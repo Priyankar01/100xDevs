a63dd656eeb1f79c074cc685cad04d6be6ed06e2
Priyankar01 <priyankardutta001@gmail.com>
2023-12-22 02:35:48 +0530

    new assignments

--- try.js
+++ try.js
@@ -1,14 +1,15 @@
-const taarik = new Date();
+function asyncFunc() {
+	let p = new Promise(function () {
+		setTimeout(function () {
+			console.log('hi there');
+		});
+	});
+	return p;
+}
 
-console.log('Date', taarik);
-console.log("Today's date:", taarik.getDate());
-console.log('Current Month:', taarik.getMonth());
-console.log('Current year:', taarik.getFullYear());
-console.log('Current hours:', taarik.getHours());
-console.log('Minutes:', taarik.getMinutes());
-console.log('Seconds:', taarik.getSeconds());
-// taarik.setFullYear(2022);
-console.log('After setFullYear:', taarik);
-// taarik.setMonth(5);
-console.log('After setMonth:', taarik);
-console.log('Time in milliseconds since 1970:', taarik.getTime());
+async function main() {
+	let value = await asyncFunc();
+	console.log(value);
+}
+
+main();
